33f43377fd67345f30728542aa71c617a62b813e
Ed Santos <edsantos2822@gmail.com>
2022-08-28 19:14:36 -0300

    (index.ts): explicando um pouco sobre o tsc

--- intro_ts_dio/intro/index.ts
+++ intro_ts_dio/intro/index.ts
@@ -5,3 +5,9 @@
 
 // 2 - Em seguida nós precisamos instalar o pacote typescript com o comando:
 // npm install typescript
+
+// Este pacote nos dá o 'tsc' que é o transpilador TypeScript/JavaScript. Um transpilador tem a função de converter código de uma linguagem de alto nível em outra de alto nível, neste caso, o 'tsc' transpila códigos TS em códigos JS.
+// O que define que um projeto é um projeto TypeScript é haver o arquivo de configuração 'tsconfig.json'.
+// Podemos gerar um arquivo de configuração automáticamente com o comando:
+// npx tsc --init
+
