a42de46286ce65b1c2670840aa8dee89a2759a51
Ed Santos <edsantos2822@gmail.com>
2022-08-28 14:49:31 -0300

    (index.ts): adicionando segundo passo

--- intro_ts_dio/intro/index.ts
+++ intro_ts_dio/intro/index.ts
@@ -2,3 +2,6 @@
 
 // 1 - Primeio, é necessário iniciar um projeto Node com o comando:
 // npm init -y (flag para fazer a criação automática do arquivo package.json)
+
+// 2 - Em seguida nós precisamos instalar o pacote typescript com o comando:
+// npm install typescript
