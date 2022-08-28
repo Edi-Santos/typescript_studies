3e2ae7f863395910bed60f057087d0a98916d0e8
Ed Santos <edsantos2822@gmail.com>
2022-08-28 19:35:00 -0300

    (index.ts): adicionando observação sobre a forma de instalação do typescript

--- intro_ts_dio/intro/index.ts
+++ intro_ts_dio/intro/index.ts
@@ -5,6 +5,8 @@
 
 // 2 - Em seguida nós precisamos instalar o pacote typescript com o comando:
 // npm install typescript
+// Obs.: Em um projeto TypeScript a boa prática é instalar o typescript como dependência de desenvolvimento:
+// npm install -D typescript
 
 // Este pacote nos dá o 'tsc' que é o transpilador TypeScript/JavaScript. Um transpilador tem a função de converter código de uma linguagem de alto nível em outra de alto nível, neste caso, o 'tsc' transpila códigos TS em códigos JS.
 // O que define que um projeto é um projeto TypeScript é haver o arquivo de configuração 'tsconfig.json'.
