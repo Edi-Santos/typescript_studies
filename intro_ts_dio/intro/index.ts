1d47c19a77356599e800d08a75161abe5d9d7408
Ed Santos <edsantos2822@gmail.com>
2022-08-28 14:09:45 -0300

    (index.ts): adicionando arquivo e primeiro passo para começar a desenvolver em TS

---
+++ intro_ts_dio/intro/index.ts
@@ -0,0 +1,4 @@
+// Iniciando os Estudos - Primeiros Passos
+
+// 1 - Primeio, é necessário iniciar um projeto Node com o comando:
+// npm init -y (flag para fazer a criação automática do arquivo package.json)
