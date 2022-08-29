ea848fa3ca970a4a35236594a702c66c1e6b3827
Ed Santos <edsantos2822@gmail.com>
2022-08-29 18:15:24 -0300

    (intefaces_e_tipos.ts): adicionando interface e função de exemplo

--- intro_ts_dio/tipos_ts/intefaces_e_tipos.ts
+++ intro_ts_dio/tipos_ts/intefaces_e_tipos.ts
@@ -1,1 +1,17 @@
 // Em TypeScript nós temos as 'Interfaces' que podem nos servir para criar e definir parâmetros para funções, por exemplo. As 'Interfaces' são como objetos. Veremos um exemplo a seguir.
+
+interface IApresentation {
+  name: string,
+  born: number,
+  profession?: string,
+};
+
+function apresentation({ name, born, profession }: IApresentation): string {
+  const age: number = 2022 - born;
+
+  if (profession) return `Meu nome é ${name}, eu tenho ${age} anos e sou ${profession}`;
+
+  return `Meu nome é ${name}, eu tenho ${age} anos.`;
+}
+
+console.log(apresentation({ name: 'Ed', born: 1997 }));
