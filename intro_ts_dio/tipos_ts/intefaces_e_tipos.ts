dffaaddfe2049675a9afc85da161a2e2fe2d8ae8
Ed Santos <edsantos2822@gmail.com>
2022-08-29 18:03:01 -0300

    (interfaces_e_tipos.ts): adicionando breve descrição sobre interfaces

---
+++ intro_ts_dio/tipos_ts/intefaces_e_tipos.ts
@@ -0,0 +1,1 @@
+// Em TypeScript nós temos as 'Interfaces' que podem nos servir para criar e definir parâmetros para funções, por exemplo. As 'Interfaces' são como objetos. Veremos um exemplo a seguir.
