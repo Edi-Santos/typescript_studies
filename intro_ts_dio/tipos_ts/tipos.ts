a7eec5c8369a88dbd06cfaf11fc14c1390b31aa4
Ed Santos <edsantos2822@gmail.com>
2022-08-29 13:06:03 -0300

    (tipos.ts): explicação básica sobre tipos em TypeScript

---
+++ intro_ts_dio/tipos_ts/tipos.ts
@@ -0,0 +1,9 @@
+// Em TypeScript, todos os tipois são subtipos de um tipo principal, o 'any'. Este tipo pode representar qualquer tipo de dado JavaScript. Em TypeScript nós temos os (subi)tipos primitivos, tipos de objeto ou parâmetros de tipo.
+// Tipos primitivos são: string, number, boolean, void, null e undefined.
+
+// string: conjunto de caracteres (definidos por aspas);
+// number: tipo numérico
+// boolean: verdadeiro ou falso
+// void: tipo vazio (usado, por exemplo, em funções que não retornam valor nenhum)
+// null: tipo nulo
+// undefined: tipo indefinido
