8ebffe659ad4c7ab945225e8e6f77a7e44a1a507
Ed Santos <edsantos2822@gmail.com>
2022-08-30 16:43:16 -0300

    (index.ts): criando classe Conta

---
+++ intro_ts_dio/dioBank_ts/index.ts
@@ -0,0 +1,11 @@
+class Conta {
+  private nomeTitular: string;
+  private saldo: number;
+
+  constructor() {
+    this.nomeTitular = 'Ed';
+    this.saldo = 0;
+
+    console.log('Bem vindo ao DIO Bank');
+  }
+}
